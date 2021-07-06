6870db5640f0c6b206c47686446974d15eaf4713
G. Maciel <52551449+GCailem@users.noreply.github.com>
2021-07-06 11:19:24 -0300

    Função para validar campo CPF

--- UnitCadastroClientes.pas
+++ UnitCadastroClientes.pas
@@ -178,7 +178,11 @@ type
     procedure EDIT_paisKeyDown(Sender: TObject; var Key: Word;
       var KeyChar: Char; Shift: TShiftState);
 
+     //Grupo de atividade: Validações.
+     function ValidarCPF(pCPF: string): Boolean;
+
   private
+
     { Private declarations }
   public
     { Public declarations }
@@ -193,7 +197,7 @@ var
 implementation
 
 uses
-   ActiveX, System.SysUtils, IdText, IdAttachmentFile;
+   ActiveX, System.SysUtils, IdText, IdAttachmentFile, Math;
 {$R *.fmx}
 
 //Grupo de atividades: Sair da aplicação.
@@ -349,7 +353,7 @@ begin
     CountErro := CountErro+1;
   end;
 
-  if (EDIT_cpf.Text = '') or (EDIT_cpf.Text.Length < 11) then
+  if (EDIT_cpf.Text = '') or (NOT (ValidarCPF(EDIT_cpf.Text))) then
   begin
     REC_cpf.Stroke.Color := TAlphaColorRec.RED;
     CountErro := CountErro+1;
@@ -597,7 +601,8 @@ begin
   end
   else
   begin
-    showmessage('Não foi possível prosseguir com o cadsatro do cliente. Favor'
+    REC_email.Stroke.Color := TAlphaColorRec.RED;
+    showmessage('Não foi possível prosseguir com o cadastro do cliente. Favor'
                  +' verificar se o email está correto.');
   end;
 end;
@@ -732,4 +737,76 @@ begin
   if ((EDIT_cidade.Text.Length = 0) or (EDIT_cidade.Text[EDIT_nome.Text.Length] = ' '))
     then KeyChar := AnsiUpperCase(KeyChar)[1];
 end;
+
+
+//Grupo de atividade: Validações.
+function TFormCadastroCliente.ValidarCPF(pCPF: string): Boolean;
+var
+  v: array [0 .. 1] of Word;
+  cpf: array [0 .. 10] of Byte;
+  I: Byte;
+begin
+  Result := False;
+
+  { Verificando se tem 11 caracteres }
+  if Length(pCPF) <> 11 then
+  begin
+    Exit;
+  end;
+
+  { Conferindo se todos dígitos são iguais }
+  if pCPF = StringOfChar('0', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('1', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('2', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('3', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('4', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('5', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('6', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('7', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('8', 11) then
+    Exit;
+
+  if pCPF = StringOfChar('9', 11) then
+    Exit;
+
+  try
+    for I := 1 to 11 do
+      cpf[I - 1] := StrToInt(pCPF[I]);
+    // Nota: Calcula o primeiro dígito de verificação.
+    v[0] := 10 * cpf[0] + 9 * cpf[1] + 8 * cpf[2];
+    v[0] := v[0] + 7 * cpf[3] + 6 * cpf[4] + 5 * cpf[5];
+    v[0] := v[0] + 4 * cpf[6] + 3 * cpf[7] + 2 * cpf[8];
+    v[0] := 11 - v[0] mod 11;
+    v[0] := IfThen(v[0] >= 10, 0, v[0]);
+    // Nota: Calcula o segundo dígito de verificação.
+    v[1] := 11 * cpf[0] + 10 * cpf[1] + 9 * cpf[2];
+    v[1] := v[1] + 8 * cpf[3] + 7 * cpf[4] + 6 * cpf[5];
+    v[1] := v[1] + 5 * cpf[6] + 4 * cpf[7] + 3 * cpf[8];
+    v[1] := v[1] + 2 * v[0];
+    v[1] := 11 - v[1] mod 11;
+    v[1] := IfThen(v[1] >= 10, 0, v[1]);
+    // Nota: Verdadeiro se os dígitos de verificação são os esperados.
+    Result := ((v[0] = cpf[9]) and (v[1] = cpf[10]));
+  except
+    on E: Exception do
+      Result := False;
+  end;
+end;
+
 end.
